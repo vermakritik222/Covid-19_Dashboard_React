a2dc8723e5e952759cf6c333f47c89a0cce0d5d7
kritik <kritikverma2002@gmail.com>
2021-11-25 20:06:06 +0530

    initial Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
+      <Dashboard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
---
+++ src/util/Requests.js
@@ -0,0 +1,4 @@
+exports.requests = {
+  fetchByCountryName:
+    "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/global?country=<<COUNTRYNAME>>",
+};
