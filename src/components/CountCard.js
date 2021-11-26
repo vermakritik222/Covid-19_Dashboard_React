24ea31e432c5720d8e86ee3a8c0ed68623d61078
kritik <kritikverma2002@gmail.com>
2021-11-26 17:54:16 +0530

    count card added

---
+++ src/components/CountCard.js
@@ -0,0 +1,100 @@
+import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
+import React from "react";
+import { Line } from "react-chartjs-2";
+import "chart.js";
+import "./sass/CountCard.scss";
+
+// Chart.register(CategoryScale)
+
+function CountCard(props) {
+  const { title, data, dec, color, graphData, graphDataLabels } = props;
+  return (
+    <div className="countCard">
+      <div className="countCard__content">
+        <h3 className="countCard__title">
+          {title}{" "}
+          <span className="countCard__title-span">
+            {dec ? (
+              <ArrowDropDown style={{ fontSize: "35px", color: "#48b375" }} />
+            ) : (
+              <ArrowDropUp color="error" style={{ fontSize: "35px" }} />
+            )}
+          </span>
+        </h3>
+        <div className="countCard_data">{data}</div>
+      </div>
+      <div className="countCard__graph">
+        <Line
+          height={90}
+          width={110}
+          data={{
+            // labels: ["red", "blue", "green", "purple", "pink"],
+            labels: graphDataLabels,
+            datasets: [
+              {
+                data: graphData,
+                fill: false,
+                backgroundColor: "#f77e7ebd",
+                borderColor: color,
+                borderWidth: 4,
+                borderCapStyle: "butt",
+                pointBorderWidth: 0,
+                pointHoverRadius: 0,
+                pointRadius: 0,
+                spanGaps: false,
+              },
+            ],
+          }}
+          options={{
+            animations: {
+              tension: {
+                duration: 1000,
+                easing: "linear",
+                from: 0,
+                to: 0.3,
+                loop: false,
+              },
+            },
+            // legend: {
+            //   display: false,
+            // },
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+            // tooltips: {
+            //   enabled: false,
+            //   callbacks: {
+            //     label: function (tooltipItem) {
+            //       return tooltipItem.yLabel;
+            //     },
+            //   },
+            // },
+            responsive: true,
+            maintainAspectRatio: false,
+            indexAxis: "x",
+            scales: {
+              x: {
+                display: false,
+                ticks: {
+                  stepSize: 2,
+                  // fixedStepSize: 0,
+                },
+              },
+              y: {
+                display: false,
+                ticks: {
+                  stepSize: 10000,
+                  // fixedStepSize: 0,
+                },
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CountCard;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,49 @@
 import React from "react";
+import CountCard from "../components/CountCard";
 import Nav from "../components/Nav";
 import "./sass/Dashboard.scss";
 
 function Dashboard() {
+  const data = [
+    0, 2200, 3400, 3600, 4500, 4800, 5100, 5500, 6700, 7800, 7200, 9900,
+  ];
+  const labels = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
   return (
     <div className="dashboard">
       <Nav />
+      <div className="dashboard__body">
+        <div className="dashboard__body-sec1">
+          <CountCard
+            title="total cases"
+            color="#fc312f"
+            data="3,09,564"
+            graphData={data}
+            graphDataLabels={labels}
+          />
+          <CountCard
+            title="recovered"
+            color="#49a571"
+            data="95,829"
+            dec
+            graphData={data}
+            graphDataLabels={labels}
+          />
+          <CountCard
+            title="active cases"
+            color="#fc312f"
+            data="1,99,564"
+            graphData={data}
+            graphDataLabels={labels}
+          />
+          <CountCard
+            title="total death"
+            color="#fc312f"
+            data="13,064"
+            graphData={data}
+            graphDataLabels={labels}
+          />
+        </div>
+      </div>
     </div>
   );
 }
